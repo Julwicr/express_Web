c1313cd68cea6ec705e8085cd45edab81e16d83d
Julien Rolland <rollandjulien13013@gmail.com>
2022-07-02 12:34:12 +0200

    js works

--- public/js/works.js
+++ public/js/works.js
@@ -1,6 +1,54 @@
-const repoContainer = document.getElementById('github-repos')
+const projectsList = document.querySelectorAll('.project-list-item');
+const worksContainer = document.querySelector('.projects-preview');
+const works = document.querySelectorAll('.projects-preview-item');
+const imgs = document.getElementsByTagName('img');
+
+const scrollable = worksContainer.scrollHeight;
+
+// console.log(projectsList);
+// print scrollable height
+addEventListener('resize', () => console.log(scrollable));
+
+
+// SCROLL event
+worksContainer.addEventListener('scroll', (e) => {
+  const scroll = worksContainer.scrollTop;
+  // console.log(scroll, imgs[0].offsetTop);
+});
+
+
+// CLICK project event
+// click ajout le height de ts les preedents project
+
+const setScroll = (order) => {
+  let toScroll = 0;
+
+  for (let i = 0; i < order; i++) {
+    const work = works[i];
+    toScroll += work.scrollHeight + (scrollable / 60);
+  }
+  worksContainer.scrollTop = toScroll;
+}
+
+// add dataset to buttons
+let countProject = 0;
+for (let i = 0; i < projectsList.length; i++) {
+  const project = projectsList[i];
+
+  project.dataset.order = countProject;
+
+  project.addEventListener('click', (e) => {
+    setScroll(e.target.dataset.order);
+    console.log(e.target.dataset.order);
+  });
+
+  countProject += 1;
+}
+
 
 // getting github repos
+const repoContainer = document.getElementById('github-repos')
+
 const getRepos = async() => {
   const response = await fetch("https://api.github.com/users/julwicr/repos");
   const repos = await response.json();
@@ -15,7 +63,3 @@ const getRepos = async() => {
   });
 }
 getRepos();
-
-const worksContainer = document.querySelector('.projects-preview');
-
-worksContainer.addEventListener('scroll', (e) => console.log(worksContainer.getBoundingClientRect()));
